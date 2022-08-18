761b71202f63c11ab7d72005432541890f84a942
zolfjk <zolfjkible@gmail.com>
2022-08-18 17:05:33 +0300

    Data and test added

---
+++ src/test/java/ru/netology/testmode/test/TestModeTest.java
@@ -0,0 +1,34 @@
+package ru.netology.testmode.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+
+
+public class TestModeTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").val(registeredUser.getLogin());
+        $("[data-test-id='password'] input").val(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[id='root']").shouldHave(Condition.text("Личный кабинет"));
+    }
+}
+
